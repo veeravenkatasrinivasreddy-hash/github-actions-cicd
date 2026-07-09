3978e455ee228ac54088da9dfdec61faa9687a26
Srinivas <veeravenkatasrinivasreddy@gmail.com>
2026-07-08 21:58:34 -0400

    code

---
+++ src/test/java/com/firstproject/githubcicdactions/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.githubcicdactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
